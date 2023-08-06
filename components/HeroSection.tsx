36f25773183a1ff277c5efcd1239309c27bb8cd5
orlowski-dev <orlowski.dev@gmail.com>
2023-08-06 17:53:52 +0200

    Added FeatureSection and Slider components

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,4 +1,4 @@
-import '@/styles/HeroSection.scss'
+import '@/styles/sections.scss'
 import { LinkButton } from './Buttons'
 import Image from 'next/image'
 
---
+++ components/Slider.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import '@/styles/Slider.scss'
+import { default as slidesData } from '@/data/slidesData.json'
+import Image from 'next/image'
+import { LinkButton } from './Buttons'
+import { useRef, useState } from 'react'
+
+interface ISlideData {
+  id: number,
+  title: string,
+  headline: string
+  content: string,
+  link?: string,
+  illustration: string
+}
+
+const Slide = (props: ISlideData) => (
+  <article className="slide">
+    <div className="illustration">
+      <Image src={props.illustration} width={536} height={346} alt={`${props.title} illustration`} />
+    </div>
+    <div className="content">
+      <h3>{props.headline}</h3>
+      <p>{props.content}</p>
+      {
+        props.link ? <div className='button-area'>
+          <LinkButton href={props.link} classList={['button', 'sh', 'blue']} textContent='More info' />
+        </div> : undefined
+      }
+    </div>
+  </article>
+)
+
+
+export const Slider = () => {
+  const handleSlideTitleClick = (index: number) => {
+    const columnGap = sliderRef.current && getComputedStyle(sliderRef.current).getPropertyValue('column-gap').replace('px', '') || 740
+
+    return `translate3d(calc(-${index * columnGap}px - ${index * 100}%), 0,0)`
+  }
+  const slides = slidesData.map((slide: ISlideData) => <Slide key={slide.id} {...slide} />)
+  const slidesTitles = slidesData.map((slide: ISlideData, index: number) => (
+    <li
+      key={slide.id}
+      onClick={() => {
+        setSliderTransform(handleSlideTitleClick(index))
+      }}
+    >{slide.title}</li>
+  ))
+  const sliderRef = useRef(null)
+  const [sliderTransform, setSliderTransform] = useState<string>(handleSlideTitleClick(0))
+  return (
+    <section className="slider-section">
+      <h2 className="visually-hidden">Slider</h2>
+      <div className="slider-controls">
+        <ul className="us-ul">
+          {slidesTitles}
+        </ul>
+      </div>
+      <div
+        className="slider-scene"
+        ref={sliderRef}
+        style={{
+          gridTemplateColumns: `repeat(${slidesTitles.length}, 100%)`,
+          transform: sliderTransform
+        }}
+      >
+        {slides}
+      </div>
+    </section>
+  )
+}
